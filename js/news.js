6c79df2b9923fe6e50f8f8b74e465abba7e7b192
769062519 <769062159.com>
2018-12-14 10:21:14 +0800

    update

---
+++ js/news.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('.tabChange li').on("click",function(){
+        $(this).addClass("active").siblings().removeClass('active')
+        $('.news-content').find('.newsBox').eq($(this).index()).css({display:'block'}).siblings().css({display: "none"})
+    })
+})
